2952e5307ae518aae122d688cc8ab6e24c582f50
JavierCollipal <jcollipal1212@gmail.com>
2020-10-10 15:50:15 -0300

    base test cases for catService->adoptOne added
    Took 11 minutes

--- test/services/cat.test.js
+++ test/services/cat.test.js
@@ -1,8 +1,13 @@
 const app = require('../../src/app');
 
-describe('\'cat\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('cat');
-    expect(service).toBeTruthy();
-  });
+describe("'cat' service", () => {
+	it('registered the service', () => {
+		const service = app.service('cat');
+		expect(service).toBeTruthy();
+	});
+	describe('catService->adoptOne', () => {
+		it('should call model->findByIdAndUpdate', () => {});
+		it('catModel->findByIdAndUpdate should be called with the id and the query config as parameters', () => {});
+		it('catModel->findByIdAndUpdate should return the modified cat', () => {});
+	});
 });
